89b357b5472c916bc115354b1fdce929285f925e
Thomas Huetter <thomas.huetter@stud.sbg.ac.at>
2018-02-08 08:53:07 +0100

    added multiset databases to the sql script

--- db/create_db.sql
+++ db/create_db.sql
@@ -79,3 +79,55 @@ CREATE TABLE allpairs_self_join (
   verification_candidates integer, -- Pairs of trees resulting after verification step of the filter.
   verification_time bigint -- TED verification time.
 );
+
+DROP TABLE IF EXISTS allpairs_multiset_baseline_self_join;
+CREATE TABLE allpairs_multiset_baseline_self_join (
+  -- Common attributes.
+  execution_id serial PRIMARY KEY,
+  experiments_version varchar(127),
+  experiments_timestamp timestamp,
+  hostname varchar(127),
+  dataset_filename varchar(127) REFERENCES dataset(filename),
+  dataset_parsing_time bigint,
+  algorithm_version varchar(127),
+  threshold decimal,
+  sum_subproblems bigint,
+  result_set_size integer,
+  sum_subproblem_optimum bigint,
+  optimum_time bigint,
+  -- Algorithm-specific attributes.
+  similarity_function varchar(127),
+  verification_algorithm varchar(31),
+  tree_to_set_time bigint,
+  filter_touched_pairs integer, -- Pairs of trees that the filter looks at.
+  filter_verification_candidates integer, -- Pairs of trees resulting from filter only.
+  filter_time bigint, -- Total time of the filter and its verification step.
+  verification_candidates integer, -- Pairs of trees resulting after verification step of the filter.
+  verification_time bigint -- TED verification time.
+);
+
+DROP TABLE IF EXISTS allpairs_multiset_dsf_self_join;
+CREATE TABLE allpairs_multiset_dsf_self_join (
+  -- Common attributes.
+  execution_id serial PRIMARY KEY,
+  experiments_version varchar(127),
+  experiments_timestamp timestamp,
+  hostname varchar(127),
+  dataset_filename varchar(127) REFERENCES dataset(filename),
+  dataset_parsing_time bigint,
+  algorithm_version varchar(127),
+  threshold decimal,
+  sum_subproblems bigint,
+  result_set_size integer,
+  sum_subproblem_optimum bigint,
+  optimum_time bigint,
+  -- Algorithm-specific attributes.
+  similarity_function varchar(127),
+  verification_algorithm varchar(31),
+  tree_to_set_time bigint,
+  filter_touched_pairs integer, -- Pairs of trees that the filter looks at.
+  filter_verification_candidates integer, -- Pairs of trees resulting from filter only.
+  filter_time bigint, -- Total time of the filter and its verification step.
+  verification_candidates integer, -- Pairs of trees resulting after verification step of the filter.
+  verification_time bigint -- TED verification time.
+);
