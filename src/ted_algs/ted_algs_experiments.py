cba4953934264ec6333025f553ead2499624b0ad
Mateusz Pawlik <mpawlik@.cosy.sbg.ac.at>
2018-09-19 18:43:05 +0200

    Working on storing ted results in db.

--- src/ted_algs/ted_algs_experiments.py
+++ src/ted_algs/ted_algs_experiments.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-'''
-    File name: experiments.py
-    Author: Thomas Huetter, Mateusz Pawlik
-    Program: Wrapper script to call ./experiments.
-'''
 
 import sys
 import argparse
@@ -13,8 +8,16 @@ import psycopg2
 import socket
 import os
 from psycopg2 import sql
+from psycopg2 import extras
 from multiprocessing import Queue, Process
 from datetime import datetime
+
+# README
+# 
+# This script executes ted experiments and stores the results in the database.
+# 
+# It ensures that the experiments binary is executed once for each combination
+# of dataset, algorithm, threshold, and mechanism.
 
 # TODO: Fix the paths depending on where is the script executed from.
 # Everything is executed from the project's root.
@@ -36,18 +39,18 @@ def get_experiments_timestamp():
     # (TIMESTAMP '2011-05-16 15:36:38');
     return datetime.now()
 
-def get_respository_version(repository_root):
+def get_respository_commit(repository_root):
     popen = subprocess.Popen(["git", "rev-parse", "HEAD"], stdout=subprocess.PIPE, cwd=repository_root)
     stdoutdata, stderrdata = popen.communicate()
     if popen.returncode != 0:
         raise Exception("non-zero return code: %d" % popen.returncode)
     return bytes.decode(stdoutdata).strip()
 
-def get_algorithm_version():
-    return get_respository_version(algorithms_repository_path)
+def get_algorithm_source_commit():
+    return get_respository_commit(algorithms_repository_path)
 
-def get_experiments_version():
-    return get_respository_version(".")
+def get_experiments_source_commit():
+    return get_respository_commit(".")
 
 def get_hostname():
     return socket.gethostname()
@@ -56,6 +59,69 @@ def print_result(table_name, values_dict):
     algorithm_params = {"algorithm" : table_name}
     values_dict.update(algorithm_params)
     print(values_dict)
+
+# http://initd.org/psycopg/docs/sql.html#module-psycopg2.sql
+def store_result(table_name, values_dict):
+    # Connect to database.
+    db = psycopg2.connect("service=ted-exp-local-test")
+    # Open a cursor to perform database operations
+    cur = db.cursor()
+    attributes = values_dict.keys()
+    query = sql.SQL("INSERT INTO {} ({}) VALUES ({})").format(
+        sql.Identifier(table_name),
+        sql.SQL(', ').join(map(sql.Identifier, attributes)),
+        sql.SQL(', ').join(map(sql.Placeholder, attributes))
+    )
+    print(query.as_string(cur))
+    cur.execute(query, values_dict)
+    db.commit()
+    # Close the cursor.
+    cur.close()
+    # Close communication with the database.
+    db.close()
+
+def store_multirow_results(table_name):
+    values = [
+        {'ted': 1.0,
+         'tree_id_1': 0,
+         'subproblems': 1,
+         'tree_id_2': 1,
+         'tree_size_2': 1,
+         'tree_size_1': 1,
+         'runtime': 0.026643},
+        {'ted': 1.0,
+         'tree_id_1': 1,
+         'subproblems': 1,
+         'tree_id_2': 2,
+         'tree_size_2': 1,
+         'tree_size_1': 1,
+         'runtime': 0.015511},
+        {'ted': 1.0,
+         'tree_id_1': 2,
+         'subproblems': 2,
+         'tree_id_2': 3,
+         'tree_size_2': 2,
+         'tree_size_1': 2,
+         'runtime': 0.021519}
+    ]
+    db = psycopg2.connect("service=ted-exp-local-test")
+    cur = db.cursor()
+    attributes = values[0].keys()
+    query = sql.SQL("INSERT INTO {} ({}) VALUES %s").format(
+        sql.Identifier(table_name),
+        sql.SQL(', ').join(map(sql.Identifier, attributes))
+    )
+    # query = "INSERT INTO ted_apted (tree_id_1, subproblems, ted, runtime, tree_size_2, tree_id_2, tree_size_1) VALUES %s"
+    print(query.as_string(cur))
+    psycopg2.extras.execute_values(
+        cur,
+        query.as_string(cur),
+        values,
+        template='(' + sql.SQL(', ').join(map(sql.Placeholder, attributes)).as_string(cur) + ')'
+    )
+    db.commit()
+    cur.close()
+    db.close()
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -66,46 +132,63 @@ parser.add_argument(
 args = parser.parse_args()
 
 # Fixed values for this execution - passed to all algorithms.
-fixed_values = {
-    "experiments_timestamp" : get_experiments_timestamp(),
-    "algorithm_version" : get_algorithm_version(),
-    "experiments_version" : get_experiments_version(),
+ted_experiment_params = {
+    "ted_experiment_timestamp" : get_experiments_timestamp(),
+    "algorithm_source_commit" : get_algorithm_source_commit(),
+    "experiments_source_commit" : get_experiments_source_commit(),
     "hostname" : get_hostname()
 }
 
-data = json.load(open(args.config_filename))
-for d in data['datasets']:
-    for t in data['thresholds']:
-        path, filename = os.path.split(d)
-        experiment_params = {
-            "dataset_filename" : filename,
-            "threshold" : t
-        }
-        # build command that needs to be executed
-        cmd = []
-        # call binary
-        # ted-algs-experiments
-        # --input
-        # ~/temp/programming/RTED/branches/tods-experiments/datasets/artificial/zz/zz-200-2000.dataset
-        # --tz --tzd --tzs --tzse --tzl --tzle --lg
-        # --one-by-one
-        # --threshold 20 --output json
-        cmd.extend([
-            binary_name,
-            '--input', str(d),
-            '--output' , 'json',
-            '--threshold', str(t),
-            data['mechanism'],
-        ])
-        cmd.extend(data['algorithms'])
-        print(cmd)
-        
-        cmd_output = get_stdout_cmd(cmd).strip()
-        result_data = json.loads(cmd_output.decode('utf-8'))
-        result_data.update(fixed_values)
-        result_data.update(experiment_params)
-        # result_data.update(algorithm_params)
-        # store_result(a['name'], result_data)
-        # print_result(a['name'], result_data)
-        print(result_data)
+ted_algs = ["--apted", "--zs"]
 
+data = json.load(open(args.config_filename))
+for a in data['algorithms']:
+    for d in data['datasets']:
+        for t in data['thresholds']:
+            path, filename = os.path.split(d)
+            ted_experiment_params.update({
+                "dataset_filename" : filename,
+                "ted_threshold" : t
+            })
+            print(ted_experiment_params)
+            
+            # build command that needs to be executed
+            cmd = []
+            # call binary
+            # ted-algs-experiments
+            # --input
+            # ~/temp/programming/RTED/branches/tods-experiments/datasets/artificial/zz/zz-200-2000.dataset
+            # --tz --tzd --tzs --tzse --tzl --tzle --lg
+            # --one-by-one
+            # --threshold 20 --output json
+            cmd.extend([
+                binary_name,
+                '--input', str(d),
+                '--output' , 'json',
+                '--threshold', str(t),
+            ])
+            if 'mechanism' in data:
+                cmd.extend([data['mechanism'],])
+            cmd.extend([a,])
+            print(cmd)
+
+            cmd_output = get_stdout_cmd(cmd).strip()
+            result_data = json.loads(cmd_output.decode('utf-8'))
+            # # result_data.update(algorithm_params)
+            # # store_result(a['name'], result_data)
+            # # print_result(a['name'], result_data)
+            # print(result_data)
+                        
+            print(result_data['algorithm_executions'][0]['data_items'])
+            
+            store_multirow_results("ted_apted")
+            
+            # TODO: Handle inf correctly.
+            
+            # TODO: Store ted_experiment_params and retrieve its id.
+            
+            # TODO: Store the results.
+            
+            # non-tedk algorithms should be executed once disregarding the thresholds
+            if a in ted_algs:
+                break
