15c15e58774700a1d82b0530cc0a452ba80f3c21
Thomas Huetter <thomas.huetter@stud.sbg.ac.at>
2019-01-10 09:12:02 +0100

    add plots for full datasets

--- src/plots/call_plot_revision.sh
+++ src/plots/call_plot_revision.sh
@@ -1,34 +1,49 @@
 ### FPR
 python3 plot_experiments.py --service ted-exp --config configs_revision/fpr/fpr_bolzano.json --storeplot "./plots/fpr/bolzano_fpr.pdf"
+python3 plot_experiments.py --service ted-exp --config configs_revision/fpr/fpr_dblp.json --storeplot "./plots/fpr/dblp_fpr.pdf"
 python3 plot_experiments.py --service ted-exp --config configs_revision/fpr/fpr_dblp_10000.json --storeplot "./plots/fpr/dblp_fpr_10000.pdf"
+python3 plot_experiments.py --service ted-exp --config configs_revision/fpr/fpr_python.json --storeplot "./plots/fpr/python_fpr.pdf"
 python3 plot_experiments.py --service ted-exp --config configs_revision/fpr/fpr_python_10000.json --storeplot "./plots/fpr/python_fpr_10000.pdf"
 python3 plot_experiments.py --service ted-exp --config configs_revision/fpr/fpr_sentiment.json --storeplot "./plots/fpr/sentiment_fpr.pdf"
+python3 plot_experiments.py --service ted-exp --config configs_revision/fpr/fpr_swissprot.json --storeplot "./plots/fpr/swissprot_fpr.pdf"
 python3 plot_experiments.py --service ted-exp --config configs_revision/fpr/fpr_swissprot_10000.json --storeplot "./plots/fpr/swissprot_fpr_10000.pdf"
 
 ### Join Times
 python3 plot_experiments.py --service ted-exp --config configs_revision/times/times_bolzano.json --storeplot "./plots/times/bolzano_times.pdf"
+python3 plot_experiments.py --service ted-exp --config configs_revision/times/times_dblp.json --storeplot "./plots/times/dblp_times.pdf"
 python3 plot_experiments.py --service ted-exp --config configs_revision/times/times_dblp_10000.json --storeplot "./plots/times/dblp_times_10000.pdf"
+python3 plot_experiments.py --service ted-exp --config configs_revision/times/times_python.json --storeplot "./plots/times/python_times.pdf"
 python3 plot_experiments.py --service ted-exp --config configs_revision/times/times_python_10000.json --storeplot "./plots/times/python_times_10000.pdf"
 python3 plot_experiments.py --service ted-exp --config configs_revision/times/times_sentiment.json --storeplot "./plots/times/sentiment_times.pdf"
+python3 plot_experiments.py --service ted-exp --config configs_revision/times/times_swissprot.json --storeplot "./plots/times/swissprot_times.pdf"
 python3 plot_experiments.py --service ted-exp --config configs_revision/times/times_swissprot_10000.json --storeplot "./plots/times/swissprot_times_10000.pdf"
 
 ### Candidate Times
 python3 plot_experiments.py --service ted-exp --config configs_revision/cand_times/cand_times_bolzano.json --storeplot "./plots/cand_times/bolzano_cand_times.pdf"
+python3 plot_experiments.py --service ted-exp --config configs_revision/cand_times/cand_times_dblp.json --storeplot "./plots/cand_times/dblp_cand_times.pdf"
 python3 plot_experiments.py --service ted-exp --config configs_revision/cand_times/cand_times_dblp_10000.json --storeplot "./plots/cand_times/dblp_cand_times_10000.pdf"
+python3 plot_experiments.py --service ted-exp --config configs_revision/cand_times/cand_times_python.json --storeplot "./plots/cand_times/python_cand_times.pdf"
 python3 plot_experiments.py --service ted-exp --config configs_revision/cand_times/cand_times_python_10000.json --storeplot "./plots/cand_times/python_cand_times_10000.pdf"
 python3 plot_experiments.py --service ted-exp --config configs_revision/cand_times/cand_times_sentiment.json --storeplot "./plots/cand_times/sentiment_cand_times.pdf"
+python3 plot_experiments.py --service ted-exp --config configs_revision/cand_times/cand_times_swissprot.json --storeplot "./plots/cand_times/swissprot_cand_times.pdf"
 python3 plot_experiments.py --service ted-exp --config configs_revision/cand_times/cand_times_swissprot_10000.json --storeplot "./plots/cand_times/swissprot_cand_times_10000.pdf"
 
 ### Candidate Times
 python3 plot_experiments.py --service ted-exp --config configs_revision/cand_times_conv/cand_times_conv_bolzano.json --storeplot "./plots/cand_times_conv/bolzano_cand_conv_times.pdf"
+python3 plot_experiments.py --service ted-exp --config configs_revision/cand_times_conv/cand_times_conv_dblp.json --storeplot "./plots/cand_times_conv/dblp_cand_conv_times.pdf"
 python3 plot_experiments.py --service ted-exp --config configs_revision/cand_times_conv/cand_times_conv_dblp_10000.json --storeplot "./plots/cand_times_conv/dblp_cand_conv_times_10000.pdf"
+python3 plot_experiments.py --service ted-exp --config configs_revision/cand_times_conv/cand_times_conv_python.json --storeplot "./plots/cand_times_conv/python_cand_conv_times.pdf"
 python3 plot_experiments.py --service ted-exp --config configs_revision/cand_times_conv/cand_times_conv_python_10000.json --storeplot "./plots/cand_times_conv/python_cand_conv_times_10000.pdf"
 python3 plot_experiments.py --service ted-exp --config configs_revision/cand_times_conv/cand_times_conv_sentiment.json --storeplot "./plots/cand_times_conv/sentiment_cand_conv_times.pdf"
+python3 plot_experiments.py --service ted-exp --config configs_revision/cand_times_conv/cand_times_conv_swissprot.json --storeplot "./plots/cand_times_conv/swissprot_cand_conv_times.pdf"
 python3 plot_experiments.py --service ted-exp --config configs_revision/cand_times_conv/cand_times_conv_swissprot_10000.json --storeplot "./plots/cand_times_conv/swissprot_cand_conv_times_10000.pdf"
 
 ### Candidates
 python3 plot_experiments.py --service ted-exp --config configs_revision/candidates/candidates_bolzano.json --storeplot "./plots/candidates/bolzano_candidates.pdf"
+python3 plot_experiments.py --service ted-exp --config configs_revision/candidates/candidates_dblp.json --storeplot "./plots/candidates/dblp_candidates.pdf"
 python3 plot_experiments.py --service ted-exp --config configs_revision/candidates/candidates_dblp_10000.json --storeplot "./plots/candidates/dblp_candidates_10000.pdf"
+python3 plot_experiments.py --service ted-exp --config configs_revision/candidates/candidates_python.json --storeplot "./plots/candidates/python_candidates.pdf"
 python3 plot_experiments.py --service ted-exp --config configs_revision/candidates/candidates_python_10000.json --storeplot "./plots/candidates/python_candidates_10000.pdf"
 python3 plot_experiments.py --service ted-exp --config configs_revision/candidates/candidates_sentiment.json --storeplot "./plots/candidates/sentiment_candidates.pdf"
+python3 plot_experiments.py --service ted-exp --config configs_revision/candidates/candidates_swissprot.json --storeplot "./plots/candidates/swissprot_candidates.pdf"
 python3 plot_experiments.py --service ted-exp --config configs_revision/candidates/candidates_swissprot_10000.json --storeplot "./plots/candidates/swissprot_candidates_10000.pdf"
