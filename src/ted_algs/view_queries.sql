7e388bbef44276cf7ddab5148d8e5c02f812d8e6
Mateusz Pawlik <mpawlik@.cosy.sbg.ac.at>
2020-03-04 09:11:12 +0000

    Deleted unused views.

--- src/ted_algs/view_queries.sql
+++ src/ted_algs/view_queries.sql
@@ -9,17 +9,6 @@ CREATE MATERIALIZED VIEW sentiment_apted_x_avg_pair_tree_size_y_avg_runtime_k10 
   ORDER BY avg_pair_tree_size
 WITH DATA;
 
--- Sentiment ted_sed average runtime per average size in tree pair k10.
-DROP MATERIALIZED VIEW IF EXISTS sentiment_sed_x_avg_pair_tree_size_y_avg_runtime_k10;
-CREATE MATERIALIZED VIEW sentiment_sed_x_avg_pair_tree_size_y_avg_runtime_k10 AS
-  SELECT (tree_size_1+tree_size_2)/2.0 AS avg_pair_tree_size, avg(runtime) AS avg_runtime
-  FROM ted_sed NATURAL JOIN ted_experiment_params
-  WHERE dataset_filename = 'sentiment_sorted.bracket'
-  AND abs(tree_size_1-tree_size_2)<=10
-  GROUP BY avg_pair_tree_size
-  ORDER BY avg_pair_tree_size
-WITH DATA;
-
 -- Sentiment ted_cted average runtime per average size in tree pair k10.
 DROP MATERIALIZED VIEW IF EXISTS sentiment_cted_x_avg_pair_tree_size_y_avg_runtime_k10;
 CREATE MATERIALIZED VIEW sentiment_cted_x_avg_pair_tree_size_y_avg_runtime_k10 AS
@@ -66,17 +55,6 @@ CREATE MATERIALIZED VIEW dblp_apted_x_avg_pair_tree_size_y_avg_runtime_k10 AS
   ORDER BY avg_pair_tree_size
 WITH DATA;
 
--- DBLP ted_sed average runtime per average size in tree pair k10.
-DROP MATERIALIZED VIEW IF EXISTS dblp_sed_x_avg_pair_tree_size_y_avg_runtime_k10;
-CREATE MATERIALIZED VIEW dblp_sed_x_avg_pair_tree_size_y_avg_runtime_k10 AS
-  SELECT (tree_size_1+tree_size_2)/2.0 AS avg_pair_tree_size, avg(runtime) AS avg_runtime
-  FROM ted_sed NATURAL JOIN ted_experiment_params
-  WHERE dataset_filename = 'dblp_no_www_sorted.bracket'
-  AND abs(tree_size_1-tree_size_2)<=10
-  GROUP BY avg_pair_tree_size
-  ORDER BY avg_pair_tree_size
-WITH DATA;
-
 -- DBLP ted_cted average runtime per average size in tree pair k10.
 DROP MATERIALIZED VIEW IF EXISTS dblp_cted_x_avg_pair_tree_size_y_avg_runtime_k10;
 CREATE MATERIALIZED VIEW dblp_cted_x_avg_pair_tree_size_y_avg_runtime_k10 AS
@@ -110,37 +88,6 @@ CREATE MATERIALIZED VIEW dblp_touzetd_x_avg_pair_tree_size_y_avg_runtime_k10 AS
   AND ted_threshold = 10
   GROUP BY avg_pair_tree_size
   ORDER BY avg_pair_tree_size
-WITH DATA;
-
--- Sentiment tedk_labelguided runtime sum per threshold.
-DROP MATERIALIZED VIEW IF EXISTS sentiment_labelguided_x_threshold_y_sum_runtime CASCADE;
-CREATE MATERIALIZED VIEW sentiment_labelguided_x_threshold_y_sum_runtime AS
-  SELECT ted_threshold AS ted_threshold, sum(runtime) AS sum_runtime
-  FROM tedk_labelguided NATURAL JOIN ted_experiment_params
-  WHERE dataset_filename = 'sentiment_sorted.bracket'
-  AND abs(tree_size_1-tree_size_2) <= ted_threshold
-  GROUP BY ted_threshold
-  ORDER BY ted_threshold
-WITH DATA;
-
--- Sentiment tedk_touzetd runtime sum per threshold.
-DROP MATERIALIZED VIEW IF EXISTS sentiment_touzetd_x_threshold_y_sum_runtime CASCADE;
-CREATE MATERIALIZED VIEW sentiment_touzetd_x_threshold_y_sum_runtime AS
-  SELECT ted_threshold as ted_threshold, sum(runtime) as sum_runtime
-  FROM tedk_touzetd NATURAL JOIN ted_experiment_params
-  WHERE dataset_filename = 'sentiment_sorted.bracket'
-  AND abs(tree_size_1-tree_size_2) <= ted_threshold
-  GROUP BY ted_threshold
-  ORDER BY ted_threshold
-WITH DATA;
-
--- Sentiment runtime factor per threshold tedk_labelguided vs. tedk_touzetd.
-DROP MATERIALIZED VIEW IF EXISTS sentiment_x_threshold_y_runtime_factor;
-CREATE MATERIALIZED VIEW sentiment_x_threshold_y_runtime_factor AS
-  SELECT l.ted_threshold, t.sum_runtime/l.sum_runtime AS runtime_factor
-  FROM sentiment_labelguided_x_threshold_y_sum_runtime AS l
-  JOIN sentiment_touzetd_x_threshold_y_sum_runtime AS t
-  ON l.ted_threshold=t.ted_threshold
 WITH DATA;
 
 -- Sentiment ted_apted average ted value per average size in tree pair k10.
