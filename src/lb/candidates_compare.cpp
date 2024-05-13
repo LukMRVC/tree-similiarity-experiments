ed5af1cbf1d1240810dc29e01740bcf3787c6449
Lukas Moravec <sydnexpklover@gmail.com>
2024-05-13 18:43:19 +0200

    Add candidates compare

---
+++ src/lb/candidates_compare.cpp
@@ -0,0 +1,176 @@
+//
+// Created by lukas on 10/3/23.
+//
+
+#include "ted_ds_dist.h"
+
+#include <thread>
+#include <fstream>
+#include <mutex>
+
+rlim_t increase_stack_size(rlim_t &);
+
+using namespace std::chrono;
+
+std::vector<std::pair<int, int>> read_candidates(std::string& candidates_path);
+int main(int argc, char *argv[])
+{
+    if (argc < 6)
+    {
+        std::cerr << "Missing arguments for DATASET, CANDIDATES1, CANDIDATES2, [apted|topdiff] THRESHOLD" << std::endl;
+        exit(1);
+    }
+
+    std::vector<std::string> args(argv + 1, argv + argc);
+
+    // path to dataset file containing trees in BN (Bracket notation)
+    std::string dataset_path = args.at(0);
+
+    std::string candidates1 = args.at(1);
+    std::string candidates2 = args.at(2);
+
+    std::string method = args.at(3);
+
+    if (!(method == "apted" || method == "topdiff")) {
+        std::cerr << "Unknown method selected, only [apted|topdiff] available\n";
+        exit(1);
+    }
+
+    auto threshold = std::stoi(args.at(4));
+
+    // now increase stack size
+    rlim_t new_stack_size = 16777216;
+    if (increase_stack_size(new_stack_size) != new_stack_size)
+    {
+        std::cerr << "Unable to increase stack size!" << std::endl;
+        exit(1);
+    }
+
+    using Label = label::StringLabel;
+    using CostModelLD = cost_model::UnitCostModelLD<Label>;
+    using LabelDictionary = label::LabelDictionary<Label>;
+    //  Use APTED to compute actual tree distances at least once between all trees <- this is going to take a lot of time
+    //  Computing all these distances is necessary, so we can determine the success rate of the filter
+    //  The computation is going to take a lot of time, might as well run it in parallel :]
+    using TedTouzet = ted::TouzetKRSetTreeIndex<CostModelLD , node::TreeIndexTouzetKRSet>;
+    using APTED = ted::APTEDTreeIndex<CostModelLD>;
+
+    // PARSE INPUT
+    // The input is parsed once for the entire experiment.
+    std::vector<node::Node<Label>> trees_collection;
+    parser::BracketNotationParser<Label> bnp;
+    std::cout << "Parsing tree collection for " << dataset_path << std::endl;
+
+    bnp.parse_collection(trees_collection, dataset_path);
+    std::cout << "Parsing done" << std::endl;
+
+    std::vector<std::pair<int, int>> candidates1vec = read_candidates(candidates1);
+    std::vector<std::pair<int, int>> candidates2vec = read_candidates(candidates2);
+
+    LabelDictionary ld;
+    CostModelLD ucm(ld);
+
+    TedTouzet top_diff(ucm);
+    APTED apted(ucm);
+    std::vector<double> teds;
+
+
+    if (method == "topdiff") {
+        std::vector<node::TreeIndexTouzetKRSet> tree_indexes;
+        for (size_t j = 0; j < trees_collection.size(); ++j) {
+            node::TreeIndexTouzetKRSet ti;
+            node::index_tree(ti, trees_collection[j], ld, ucm);
+            tree_indexes.emplace_back(ti);
+        }
+        auto start  = high_resolution_clock ::now();
+        for (const auto & [t1, t2] : candidates1vec) {
+            teds.emplace_back(top_diff.ted_k(tree_indexes[t1], tree_indexes[t2], threshold));
+        }
+
+        auto stop = high_resolution_clock ::now();
+        auto duration1 = duration_cast<milliseconds>(stop - start);
+        std::cout << "First candidates duration: " << duration1.count() << "ms" << std::endl;
+        start  = high_resolution_clock ::now();
+        teds.clear();
+        for (const auto & [t1, t2] : candidates2vec) {
+            teds.emplace_back(top_diff.ted_k(tree_indexes[t1], tree_indexes[t2], threshold));
+
+        }
+        stop = high_resolution_clock ::now();
+        auto duration2 = duration_cast<milliseconds>(stop - start);
+        std::cout << "Second candidates duration: " << duration2.count() << "ms" << std::endl;
+        std::cout << "The execution difference is " << duration1.count() - duration2.count() << "ms" << std::endl;
+    } else {
+        std::vector<node::TreeIndexAPTED> tree_indexes;
+        for (size_t j = 0; j < trees_collection.size(); ++j) {
+            node::TreeIndexAPTED ti;
+            node::index_tree(ti, trees_collection[j], ld, ucm);
+            tree_indexes.emplace_back(ti);
+        }
+        auto start  = high_resolution_clock ::now();
+        for (const auto & [t1, t2] : candidates1vec) {
+            teds.emplace_back(apted.ted(tree_indexes[t1], tree_indexes[t2]));
+        }
+
+        auto stop = high_resolution_clock ::now();
+        auto duration1 = duration_cast<milliseconds>(stop - start);
+        std::cout << "First candidates duration: " << duration1.count() << "ms" << std::endl;
+        start  = high_resolution_clock ::now();
+        teds.clear();
+        for (const auto & [t1, t2] : candidates2vec) {
+            teds.emplace_back(apted.ted(tree_indexes[t1], tree_indexes[t2]));
+
+        }
+        stop = high_resolution_clock ::now();
+        auto duration2 = duration_cast<milliseconds>(stop - start);
+        std::cout << "Second candidates duration: " << duration2.count() << "ms" << std::endl;
+        std::cout << "The execution difference is " << duration1.count() - duration2.count() << "ms" << std::endl;
+    }
+
+    return 0;
+}
+
+std::vector<std::pair<int, int>> read_candidates(std::string& candidates_path) {
+    std::ifstream cfile(candidates_path);
+    std::vector<std::pair<int, int>> candidatesvec;
+
+    std::string line;
+    std::string tmp;
+    std::ifstream results(candidates_path);
+    size_t t1;
+    size_t t2;
+
+    std::vector<std::vector<size_t>> results_map;
+
+    while (std::getline(results, line)) {
+        auto ls = std::stringstream(line);
+        std::getline(ls, tmp, ',');
+        t1 = std::stoul(tmp);
+
+        std::getline(ls, tmp, '\n');
+        t2 = std::stoul(tmp);
+
+        candidatesvec.emplace_back(t1, t2);
+    }
+
+    return candidatesvec;
+}
+
+rlim_t increase_stack_size(rlim_t &new_stack_size)
+{
+    // Define and object of structure rlimit.
+    struct rlimit rl;
+    // First get the time limit on CPU
+    getrlimit(RLIMIT_STACK, &rl);
+    // printf("\n Default value is : %lld\n", (long long int)rl.rlim_cur);
+    // Change the time limit
+    rl.rlim_cur = new_stack_size;
+    // Now call setrlimit() to set the
+    // changed value.
+    setrlimit(RLIMIT_STACK, &rl);
+    // Again get the limit and check
+    getrlimit(RLIMIT_STACK, &rl);
+    // printf("\n Default value now is : %lld\n", (long long int)rl.rlim_cur);
+    return rl.rlim_cur;
+}
+
